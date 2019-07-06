ad6084eb4530f476b0cf00914cd80bdc4349c064
Rainbo <jesselyn.jackson@gmail.com>
2019-07-05 22:15:30 -0400

    Check for API functionality

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,1 +1,14 @@
-//array of topics
+//Giphy API key: nt3uXtGPz8eT3lTQYpiN0o6h56RubV6E
+let athlete = "Michael Jordan";
+let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + athlete + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+    var results = response.data;
+    let gif = $("<img>");
+    gif.attr("src", results[0].images.fixed_height.url);
+    $("#gifs").append(gif);
+});
