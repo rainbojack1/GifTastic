a63c2f4d03de196ca8b9cc77b7b8ffe1aed4230b
Rainbo <jesselyn.jackson@gmail.com>
2019-07-06 09:41:01 -0400

    Created an array of athletes

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,5 +1,7 @@
 //Giphy API key: nt3uXtGPz8eT3lTQYpiN0o6h56RubV6E
-let athlete = "Michael Jordan";
+let topicArr = ["Michael Jordan", "Larry Bird", "Lebron James", "Kobe Bryant", "Simone Biles", "Shannon Miller", "Gabby Douglas", "Ussain Bolt", "Carl Lewis", "Florence Griffth Joyner", "Tiger Woods", "Phil Mickelson", "Venus Williams", "Serena Williams", "Rafael Nadal", "Naomi Osaka", "David Beckham", "Lionel Messi"]
+
+let athlete = "";
 let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + athlete + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 $.ajax({
